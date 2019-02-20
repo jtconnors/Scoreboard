6691638b0e90a9129d93d733a91e50811a2b5bdd
Jim Connors <github@jtconnors.com>
2019-02-19 21:21:47 -0500

    Added support for mac scripts run.sh, run-simplified.sh, link.sh

--- run-simplified.bat
+++ run-simplified.bat
@@ -10,9 +10,9 @@ SET MAINCLASS=com.jtconnors.scoreboard.fx2.Main
 
 SET MODPATH=%TARGET%;%REPO%com\jtconnors\com.jtconnors.socket\11.0.3\com.jtconnors.socket-11.0.3.jar;%REPO%org\openjfx\javafx-base\11.0.1\javafx-base-11.0.1.jar;%REPO%org\openjfx\javafx-controls\11.0.1\javafx-controls-11.0.1.jar;%REPO%org\openjfx\javafx-fxml\11.0.1\javafx-fxml-11.0.1.jar;%REPO%org\openjfx\javafx-graphics\11.0.1\javafx-graphics-11.0.1.jar;%REPO%org\openjfx\javafx-media\11.0.2\javafx-media-11.0.2.jar;%REPO%org\openjfx\javafx-base\11.0.1\javafx-base-11.0.1-win.jar;%REPO%org\openjfx\javafx-controls\11.0.1\javafx-controls-11.0.1-win.jar;%REPO%org\openjfx\javafx-fxml\11.0.1\javafx-fxml-11.0.1-win.jar;%REPO%org\openjfx\javafx-graphics\11.0.1\javafx-graphics-11.0.1-win.jar;%REPO%org\openjfx\javafx-media\11.0.2\javafx-media-11.0.2-win.jar
 
-REM
-REM may need to run 'mvn jar:jar' first to create the %TARGET%
-REM
+ECHO
+ECHO Did you run 'mvn jar:jar' first?
+ECHO
 
 jar --main-class %MAINCLASS% --update --file %TARGET%
 
