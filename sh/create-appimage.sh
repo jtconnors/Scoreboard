35a1e5c5f409f953feefb0c6419c0e9977d1f01e
Jim Connors <github@jtconnors.com>
2019-03-01 15:59:03 -0500

    Reorg sh/ and ps1\, rename create-image.{sh,ps1} to create-appimage.{sh,ps1}

--- sh/create-appimage.sh
+++ sh/create-appimage.sh
@@ -11,4 +11,13 @@ cd `dirname $0`
 #
 . env.sh
 
+#
+# Have to manually specify main class via jar --update until
+# maven-jar-plugin 3.1.2+ is released
+if [ ! -f $TARGET/$MAINJAR ] ; then
+	echo "Did you run 'mvn package' first?"
+	exit 1
+fi
+exec_cmd "jar --main-class $MAINCLASS --update --file $TARGET/$MAINJAR"
+
 exec_cmd "$JPACKAGE_HOME/bin/jpackage create-image $VERBOSE_OPTION --runtime-image $IMAGE --input $TARGET --output $APPIMAGE --name $LAUNCHER --main-jar $MAINJAR"
--- sh/env.sh
+++ sh/env.sh
@@ -1,15 +1,16 @@
 
 #
-# These variables must refer to the project name and the directory
-# in the filesystem where the project resides respectively
-#
-PROJECT=Scoreboard
-PROJECTDIR=~/NetBeansProjects/$PROJECT
-
-#
-# Location of JDK with jpackage utility
+# Location of JDK with jpackage utility.  We defult to the user's Download
+# directory.  If it's in a different place, this variable must be changed.
 #
 JPACKAGE_HOME=~/Downloads/jdk-13.jdk/Contents/Home
+
+#
+# Unless these script files have been deliberately moved, the parent
+# directory of the directory containining these script files houses
+# the maven project and source code.
+#
+PROJECTDIR=..
 
 #
 # native platform
@@ -20,6 +21,7 @@ INSTALLER_TYPE=dmg
 #
 # Application specific variables
 #
+PROJECT=Scoreboard
 MAINMODULE=scoreboard
 MAINCLASS=com.jtconnors.scoreboard.fx2.Main
 MAINJAR=Scoreboard-1.2.jar
